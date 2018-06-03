a022d96b04f3c2c88c0cea6fbfd711a5d2fe5e54
Ledmonds <39761293+Ledmonds@users.noreply.github.com>
2018-06-03 15:06:29 +0200

    Add files via upload
    Changes:
    Variable renaming for consistency.
    Implementation of Board class.
    Fixed static variable implementation in class definitions.
    Basic Direction and score systems implemented.
    Implemented board class to track changes in board state.
    Recoded the Snake.isSnakebodyMember() function.
    
    To Action:
    Implement movement controls, thread this too please.
    Move separate classes to their own class files and create a make file to compile everything.
    Need to implement error checking for invalid user input. For example board too large, or input outside bounds of arrays.
    Remove sleep function. Implement Delta time instead
    Maybe create a 2D vector class for storing coordinates, vector class seems a little too difficult / fancy for this game.
    
    Known Bugs:
    It appears food can sometimes randomly generate outside of snake bounds, probably an error in the rand() function on my part.

--- main.cpp
+++ main.cpp
@@ -12,72 +12,55 @@ using namespace std;
 	Implement movement controls, thread this too please.
 	Implement board class to contain member fucntions for print board.
 	Implement sleep function in seperate thread.
-	Recode the isMember function, i don't like the way this looks so much. Maybe hash maps with directions and opposites stored.
+	Recode the isSnakebodyMember function, i don't like the way this looks so much. Maybe hash maps with directions and opposites stored.
 	Maybe create a 2D vector class for storing coordinates, vector class seems a little too difficult / fancy for this game.
 	Implement score system.
 */
 
 
-int const  WINDOW_X = 80;
+int const  WINDOW_X = 150;
 int const WINDOW_Y = 38;
-int const UPDATE_TIME = 500; //ms
+int const UPDATE_TIME = 100; //ms
+
 
 /*==========
 FOOD BEGIN
 ==========*/
 class Food {
 	private:
-		int x;
-		int y;
-		char food_character;
-
-		void generateRandomFoodCoOrds() {
-			x = rand() % WINDOW_X-1 + 1;
-			y = rand() % WINDOW_Y-1 + 1;
-		}
+		int food_x;
+		int food_y;
+		static const char food_character = '+';
 
 	public:
 		//Constructors
 		Food();
 		Food(int,int);
-		Food(int,int,char);
 
 		//Gettors:
-		int getFoodX() {return x;}
-		int getFoodY() {return y;}
+		int getFoodX() {return food_x;}
+		int getFoodY() {return food_y;}
 		char getFoodCharacter() {return food_character;}
 
-		//Settors
-		void setFoodX(int _x) {x = _x;}
-		void setFoodY(int _y) {y = _y;}
-		void setFoodCharacter(char _food_character) {food_character = _food_character;}
-
 		//Checkers
-		bool isFoodCoOds(int _potential_x, int _potential_y) {
-			return (_potential_x == x && _potential_y == y) ? true : false;
+		bool isFoodCoOds(int _potential_x, int _potential_y) {return (_potential_x == food_x && _potential_y == food_y) ? true : false;}
+
+		//Assistors
+		void generateRandomFoodCoOrds() {
+			food_x = (rand() % WINDOW_X-1) + 1;
+			food_y = (rand() % WINDOW_Y-1) + 1;
 		}
 };
-
 Food::Food() {
 	generateRandomFoodCoOrds();
-	food_character = '*';
 }
-Food::Food(int _x, int _y) {
-	x = _x;
-	y = _y;
-	food_character = '*';
+Food::Food(int _food_x, int _food_y) {
+	food_x = _food_x;
+	food_y = _food_y;
 }
-Food::Food(int _x, int _y, char _food_character) {
-	x = _x;
-	y = _y;
-	food_character = _food_character;	
-}
-
-
 /*==========
 FOOD END
 ==========*/
-
 
 
 /*==========
@@ -88,30 +71,29 @@ class Snake {
 		int bearing;
 		list<pair<int,int> >  snake_body;
 		unordered_map<int,int> bearing_map; //Holds reveresed bearings for snake directions.
-		char snakehead_character;
-		char snakebody_character;
+		static const char snakehead_character = 'O';
+		static const char snakebody_character = 'o';
 
 	public:
 		//Constructors
 		Snake();
-		Snake(int,int);
 
 		//Gettors
 		int getSnakeheadX() {return snake_body.front().first;}
 		int getSnakeheadY() {return snake_body.front().second;}
 		int getSnakeLength() {return snake_body.size()-1;} //Head not included in score.
 		int getSnakeBearing() {return bearing;}
-		string getCardinalBearing(int _bearing) {
-			if (_bearing == 0) return "North";
-			else if (_bearing == 1) return "West";
-			else if (_bearing == 2) return "South";
+		string getSnakeCardinalBearing() {
+			if (bearing == 0) return "North";
+			else if (bearing == 1) return "West";
+			else if (bearing == 2) return "South";
 			else return "East";
 		}		
 		char getSnakeheadCharacter() {return snakehead_character;}
 		char getSnakebodyCharacter() {return snakebody_character;}
 
 		//Checkers
-		bool isMember(int _potential_x, int _potential_y) {
+		bool isSnakebodyMember(int _potential_x, int _potential_y) {
 			if (snake_body.size() <= 1) return false;
 			else {
 				list <pair<int,int> >::iterator it = snake_body.begin();
@@ -130,7 +112,7 @@ class Snake {
 		}
 
 		bool isBodyCollision() {
-			return isMember(snake_body.front().first,snake_body.front().second);
+			return isSnakebodyMember(snake_body.front().first,snake_body.front().second);
 		}
 
 		bool isValidBearingChange(int _potential_bearing) {
@@ -182,58 +164,102 @@ class Snake {
 
 Snake::Snake() {
 	snake_body.push_back(make_pair(WINDOW_X/2,WINDOW_Y/2));
-	generateRandomSnakeDelta(3);
+	generateRandomSnakeDelta(4);
 	generateBearingMap();
 	bearing = 0;
-	snakehead_character = 'O';
-	snakebody_character = 'o';
-	
-	
 }
-
-Snake::Snake(int _headX, int _headY) {
-	snake_body.push_back(make_pair(_headY,_headX));
-	generateBearingMap();
-	bearing = 0;
-	snakehead_character = 'O';
-	snakebody_character = 'o';	
-}
-
-
 /*==========
 SNAKE END
 ==========*/
 
-void printBoard(Snake & _snake, Food & _food) {
-	for(int y = 0; y < WINDOW_Y; ++y) {
-		for(int x = 0; x < WINDOW_X; ++x) {
-			if(x == 0 || y == 0 || x == WINDOW_X-1 || y == WINDOW_Y-1) cout << "*"; //isBorder
-			else if (_snake.isSnakehead(x,y)) cout << _snake.getSnakeheadCharacter();
-			else if (_snake.isMember(x,y)) cout << _snake.getSnakebodyCharacter();
-			else if (_food.isFoodCoOds(x,y)) cout << _food.getFoodCharacter();
-			else cout << " "; //isNotBorder
+/*==========
+BOARD BEGIN
+==========*/
+class Board {
+	private:
+		int board_x;
+		int board_y;
+		int score;
+		static const char boarder_character = '*';
+		Snake snake;
+		Food food;
+
+	public:
+		//Constructors
+		Board();
+
+		//Gettors
+		int getBoardX() {return board_x;}
+		int getBoardY() {return board_y;}
+		int getScore() {return score;}
+		char getBoarderCharacter() {return boarder_character;}
+		Snake getSnake() {return snake;}
+		Food getFood() {return food;}
+
+		//Checkers
+		bool isWall(int _potential_x, int _potential_y) {
+			return (_potential_x == 0 || _potential_x == board_x-1 || _potential_y == 0 || _potential_y == board_y-1) ? true : false;
 		}
-		cout << endl;
-	}
+		bool boardCollisionCheck() {
+			return (snake.isWallCollision() || snake.isBodyCollision());
+		}
+
+		//Assistors
+		void printBoard() {
+			system("clear");
+			for(int y = 0; y < board_y; ++y) {
+				for(int x = 0; x < board_x; ++x) {
+					if(isWall(x,y)) cout << boarder_character; //isBorder
+					else if (snake.isSnakehead(x,y)) cout << snake.getSnakeheadCharacter();
+					else if (snake.isSnakebodyMember(x,y)) cout << snake.getSnakebodyCharacter();
+					else if (food.isFoodCoOds(x,y)) cout << food.getFoodCharacter();
+					else cout << " "; //isNotBorder
+				}
+				cout << endl;
+			}
+			cout << "Score: " << score << ", Bearing: " << snake.getSnakeCardinalBearing() << endl;
+		}
+
+		void iterateBoard() {
+			this_thread::sleep_for(std::chrono::milliseconds(UPDATE_TIME)); //BADDDDDD
+
+			//snake.generateRandomSnakeDirection();
+
+			snake.iterateSnake();
+			if (snake.getSnakeheadX() == food.getFoodX() && snake.getSnakeheadY() == food.getFoodY()) {
+				snake.growSnake();
+				++score;
+				food.generateRandomFoodCoOrds();
+			}
+		}
+
+
+};
+Board::Board() {
+	board_x = WINDOW_X;
+	board_y = WINDOW_Y;
+	score = 0;
+	food = Food(WINDOW_X/2,WINDOW_Y/4);
 }
+
+/*==========
+BOARD END
+==========*/
+
 
 
 int main() {
 
 	srand(time(NULL));
 
-	Snake snake;
-	Food food;
+	Board board;
 
-	while(!snake.isWallCollision() && !snake.isBodyCollision()) {
-		system("clear");
-		printBoard(snake, food);
-		cout << "Bearing: " << snake.getCardinalBearing(snake.getSnakeBearing()) << endl;
-		this_thread::sleep_for(std::chrono::milliseconds(UPDATE_TIME)); //BADDDDDD
-		snake.generateRandomSnakeDirection();
-		snake.iterateSnake();
+	while(!board.boardCollisionCheck()) {
+		board.printBoard();
+		board.iterateBoard();
 	}
 
 	cout << "Snakey Died :(" << endl;
+
 	return 0;
 }
